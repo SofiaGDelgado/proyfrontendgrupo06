bab8b752de95f0bf4452312173338dd71d407289
Enzo Castillo <93060695+enzopcastillo@users.noreply.github.com>
2022-06-30 20:50:55 -0300

    Update empleado.service.ts

--- src/app/services/empleado.service.ts
+++ src/app/services/empleado.service.ts
@@ -8,7 +8,7 @@ import { Empleado } from '../models/empleado';
 })
 export class EmpleadoService {
 
-  urlBase= "http://localhost:4000/api/empleados/"
+  urlBase= "http://localhost:4000/api/empleado/"
 
   constructor(private http: HttpClient) { }
 
