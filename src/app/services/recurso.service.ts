41486b2d3706a70c5cef40e51a0fbbb8ef699a37
zoemarcial2001 <zoemarcial2001@gmail.com>
2022-07-03 11:22:07 -0300

    agregado tabla empleados y solucionado problema notificaciones

--- src/app/services/recurso.service.ts
+++ src/app/services/recurso.service.ts
@@ -8,7 +8,7 @@ import { Recurso } from '../models/recurso';
 })
 export class RecursoService {
 
-  urlBase = "http://localhost:4000/api/recurso";
+  urlBase = "http://localhost:4000/api/recurso/";
 
   constructor(private http: HttpClient) { }
 
@@ -32,5 +32,15 @@ export class RecursoService {
     }
 
     return this.http.get(this.urlBase, httpOption)
+  }
+
+  deleteRecurso(r: Recurso): Observable<any>{
+    const httpOption = {
+      headers: new HttpHeaders({
+        
+      })
+    }
+
+    return this.http.delete(this.urlBase+r._id, httpOption)
   }
 }
