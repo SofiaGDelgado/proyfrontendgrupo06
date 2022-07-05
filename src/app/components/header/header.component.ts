b007147b110ac22da8f1d2bbf915dc8c93011b37
zoemarcial2001 <zoemarcial2001@gmail.com>
2022-07-05 01:04:56 -0300

    agregado notificaciones del usuario y filtro por mas recientes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { parse } from 'date-fns';
+import { Empleado } from 'src/app/models/empleado';
+import { Notificacion } from 'src/app/models/notificacion';
+import { EmpleadoService } from 'src/app/services/empleado.service';
 import { LoginService } from 'src/app/services/login.service';
+import { NotificacionService } from 'src/app/services/notificacion.service';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +14,14 @@ import { LoginService } from 'src/app/services/login.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private router: Router, public loginService: LoginService) {}
+  empleadoSesion!: Empleado; 
+  notificacionesActivas!: Array<Notificacion>;
+
+  constructor(private router: Router, public loginService: LoginService, private empService: EmpleadoService) {
+    this.empleadoSesion = new Empleado();
+    this.empleadoEnSesion();
+    
+  }
 
   ngOnInit(): void {}
 
@@ -19,5 +31,48 @@ export class HeaderComponent implements OnInit {
 
   logout(){
     this.loginService.logout();
+    this.empleadoSesion = new Empleado();
+    this.notificacionesActivas = new Array<Notificacion>();
   }
+
+  empleadoEnSesion(){
+    this.empService.getEmpleados().subscribe((e) => {
+      for(var i=0; i < e.length; i++){
+        if(e[i].username === this.loginService.userLogged()){
+          Object.assign(this.empleadoSesion, e[i]);
+        }
+      }
+      console.log("en sesion: ", this.empleadoSesion);
+
+      this.cargarNotificacionesRecientes();
+    });
+
+    console.log(this.empleadoSesion.notificaciones);
+  }
+  
+  cargarNotificacionesRecientes(){
+    this.notificacionesActivas = new Array<Notificacion>();
+    console.log(this.empleadoSesion.notificaciones);
+    for(var i=0; i < this.empleadoSesion.notificaciones.length; i++){
+      var fechaNotificacion = this.empleadoSesion.notificaciones[i].fecha.split("-");
+      var fechaV = this.empleadoSesion.notificaciones[i].fechaVencimiento.split("-");
+      var fechaCompar = new Date(parseInt(fechaNotificacion[0]), parseInt(fechaNotificacion[1]), parseInt(fechaNotificacion[2]));
+      var fechaComparV = new Date(parseInt(fechaV[0]), parseInt(fechaV[1]), parseInt(fechaV[2]));
+      console.log(fechaCompar);
+      var hoy = new Date;
+      console.log(hoy);
+      if(fechaCompar.getMonth() === hoy.getMonth() && fechaCompar.getFullYear() === hoy.getFullYear()){
+          this.notificacionesActivas.push(this.empleadoSesion.notificaciones[i]);
+      }
+      else{
+        if(fechaComparV > hoy){
+          this.notificacionesActivas.push(this.empleadoSesion.notificaciones[i]);
+        }
+      }
+      
+      console.log(this.notificacionesActivas);
+    }
+  }
+
+
 }
